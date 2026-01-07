1b3596b758e65e9e19a2a244bff182eea241285f
yoriel735 <yoriellorenzo345@gmail.com>
2026-01-07 21:46:51 +0000

    hito3 error para rollback 3

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-// index.js
-function initApp() {
+// sólo en producción para no romper los tests locales
+if (process.env.NODE_ENV === 'production') {
   setTimeout(() => {
     throw new Error("Fallo intencionado Hito 3");
-  }, 1000);
+  }, 2000); // falla después de 2s, para que tests y build pasen
 }
-
-function resta(a, b) {
-  return a - b;
-}
-
-module.exports = { resta, initApp };

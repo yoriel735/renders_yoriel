541b0e407d860be0aa6ec1a0b239551f295430cc
yoriel735 <yoriellorenzo345@gmail.com>
2026-01-07 21:33:07 +0000

    hito3 error para rollback 2

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 // index.js
-console.log("App iniciando...");
-
-// Error intencionado solo al ejecutar
-setTimeout(() => {
-  throw new Error("Fallo intencionado Hito 3");
-}, 1000);
+function initApp() {
+  setTimeout(() => {
+    throw new Error("Fallo intencionado Hito 3");
+  }, 1000);
+}
 
 function resta(a, b) {
   return a - b;
 }
 
-module.exports = { resta };
+module.exports = { resta, initApp };
